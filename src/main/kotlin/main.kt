9b322e271bee04eb52db8750fce06dcf37b0bcae
Br-Silva <br_hxcx@hotmail.com>
2022-06-20 22:30:03 -0300

    Criando ContaCorrente, ContaPoupanca e Analista e utilizando polimorfismo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,56 +1,39 @@
 fun main() {
     println("Bem-vindo ao Bytebank!")
 
-    val bruno = Funcionario(
-        nome = "Bruno",
-        cpf = "123.456.789-10",
-        salario = 1000.0
+    val contaCorrente: Conta = ContaCorrente(
+        titular = "Bruno",
+        numero = 1000,
+    )
+    val contaPoupanca: Conta = ContaPoupanca(
+        titular = "Nanna",
+        numero = 1001,
     )
 
-    println("nome: ${bruno.nome}")
-    println("cpf: ${bruno.cpf}")
-    println("salário: ${bruno.salario}")
-    println("bonificação: ${bruno.bonificacao()}")
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
 
-    val nanna = Gerente(
-        nome = "Nanna",
-        cpf = "098.765.432-10",
-        salario = 2000.0,
-        senha = 1234
-    )
+    println("saldo conta corrente: ${contaCorrente.saldo}")
+    println("saldo conta poupança: ${contaPoupanca.saldo}")
 
-    println("nome: ${nanna.nome}")
-    println("cpf: ${nanna.cpf}")
-    println("salário: ${nanna.salario}")
-    println("bonificação: ${nanna.bonificacao()}")
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
 
-    if(nanna.autentica(1234)){
-        println("autenticou com sucesso")
-    } else {
-        println("falha na autenticação")
-    }
+    println("saldo após saque conta corrente: ${contaCorrente.saldo}")
+    println("saldo após saque conta poupança: ${contaPoupanca.saldo}")
 
-    val alex = Diretor(
-        nome = "Alex",
-        cpf = "111.111.111-11",
-        salario = 4000.0,
-        senha = 4000,
-        plr = 2000.0
-    )
 
-    println("nome: ${alex.nome}")
-    println("cpf: ${alex.cpf}")
-    println("salário: ${alex.salario}")
-    println("bonificação: ${alex.bonificacao()}")
-    println("plr ${alex.plr}")
+    contaCorrente.transfere(100.0, contaPoupanca)
 
-    if(alex.autentica(4000)){
-        println("autenticou com sucesso")
-    } else {
-        println("falha na autenticação")
-    }
+    println("saldo conta corrente após transferir para poupança: ${contaCorrente.saldo}")
+    println("saldo conta poupança após receber transferência: ${contaPoupanca.saldo}")
 
+    contaPoupanca.transfere(200.0, contaCorrente)
+
+    println("saldo conta poupança após transferir para corrente: ${contaPoupanca.saldo}")
+    println("saldo conta corrente após receber transferência: ${contaCorrente.saldo}")
 }
+
 
 
 
---
+++ src/main/kotlin/testaFuncionários.kt
@@ -0,0 +1,71 @@
+fun testaFuncionarios() {
+
+    val bruno = Funcionario(
+        nome = "Bruno",
+        cpf = "123.456.789-10",
+        salario = 1000.0
+    )
+
+    println("nome: ${bruno.nome}")
+    println("cpf: ${bruno.cpf}")
+    println("salário: ${bruno.salario}")
+    println("bonificação: ${bruno.bonificacao()}")
+
+    val nanna = Gerente(
+        nome = "Nanna",
+        cpf = "098.765.432-10",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome: ${nanna.nome}")
+    println("cpf: ${nanna.cpf}")
+    println("salário: ${nanna.salario}")
+    println("bonificação: ${nanna.bonificacao()}")
+
+    if (nanna.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
+
+    val alex = Diretor(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 2000.0
+    )
+
+    println("nome: ${alex.nome}")
+    println("cpf: ${alex.cpf}")
+    println("salário: ${alex.salario}")
+    println("bonificação: ${alex.bonificacao()}")
+    println("plr ${alex.plr}")
+
+    if (alex.autentica(4000)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
+
+    val maria = Analista(
+        nome = "Maria",
+        cpf = "666.666.666-66",
+        salario = 3000.0
+    )
+
+    println("nome: ${maria.nome}")
+    println("cpf: ${maria.cpf}")
+    println("salário: ${maria.salario}")
+    println("bonificação: ${maria.bonificacao()}")
+
+
+    val calculadora = CalculadoraBonificacao()
+    calculadora.registra(bruno)
+    calculadora.registra(nanna)
+    calculadora.registra(alex)
+    calculadora.registra(maria)
+
+    println("total de bonificação: ${calculadora.total}")
+}
